37e20210f2f7fa30dece09ae2e318e02a71373c2
tyrannosauruswarrior <102931365+tyrannosauruswarrior@users.noreply.github.com>
2026-03-25 09:11:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# ======================== FanchmWrt 固件自定义配置 ========================
-# 1. 修改默认LAN IP（如需改回192.168.1.1，把188改成1即可）
+# 修改默认管理IP为192.168.188.1（如需改回192.168.1.1，把188改成1即可）
 sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_generate
-
-# 2. 设置默认主机名
-sed -i 's/OpenWrt/FanchmWrt-JDCloud/g' package/base-files/files/bin/config_generate
-
-# 3. 开机默认启用WiFi
+# 开机默认启用WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
