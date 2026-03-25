a1c457a54e244c53ab2d5f2aab536897190cedad
tyrannosauruswarrior <102931365+tyrannosauruswarrior@users.noreply.github.com>
2026-03-25 09:11:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# 保留QModem源
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-
-# 添加Mihomo(Nikki)官方源
+# 添加Mihomo(Nikki)代理插件官方源
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
