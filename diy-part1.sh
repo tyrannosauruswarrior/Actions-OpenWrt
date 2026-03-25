46422627ef4d7cdb565e94f38132b339900e2ad2
tyrannosauruswarrior <102931365+tyrannosauruswarrior@users.noreply.github.com>
2026-03-25 09:00:28 +0800

    Update comments and feed sources in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,21 +4,9 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 保留原有QModem插件源
+# 保留QModem源
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 
-# 添加你要求的 Mihomo(Nikki) 代理插件官方源
+# 添加Mihomo(Nikki)官方源
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
